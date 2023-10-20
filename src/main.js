32fd94bb2a3d700d7f57f90b10629917d98f7025
ToriKeeling <tori.keeling@gmail.com>
2023-10-19 19:36:08 -0500

    Added Create Action

--- src/main.js
+++ src/main.js
@@ -6,3 +6,5 @@ import axios from "axios";
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:5000" : "/";
 
 createApp(App).mount("#app");
+
+axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
